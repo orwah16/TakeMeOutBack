717242a3a8c30b98034525ffe46c515afd2622d2
orwah <orwah_wattad@hotmail.com>
2024-01-14 17:28:36 +0200

    adding terraform for eks project

---
+++ terraform/eks_worker_nodes.tf
@@ -0,0 +1,99 @@
+resource "aws_iam_role" "node" {
+  name = "terraform-eks-node"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.EKS.name
+  node_group_name = "nodes"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = aws_subnet.EKS_private_subnet[*].id
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 1
+  }
+  ami_type       = "AL2_x86_64"
+  # instance_types = ["t3.micro"]
+  # capacity_type  = "ON_DEMAND"
+  # disk_size      = 20
+  launch_template {
+    name    = aws_launch_template.eks_launch_template.name
+    version = aws_launch_template.eks_launch_template.latest_version
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+
+
+
+resource "aws_launch_template" "eks_launch_template" {
+  name = "eks_launch_template"
+
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  #image_id = data.aws_ami.server_ami.id
+  #image_id      = "ami-03eaa1eb8976e21a9"
+  instance_type = "t2.micro"
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 5
+      volume_type = "gp3"
+    }
+  }
+
+
+  #   user_data = base64encode(<<-EOF
+  # MIME-Version: 1.0
+  # Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
+  # --==MYBOUNDARY==
+  # Content-Type: text/x-shellscript; charset="us-ascii"
+  # #!/bin/bash
+  # /etc/eks/bootstrap.sh your-eks-cluster
+  # --==MYBOUNDARY==--\
+  #   EOF
+  #   )
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "EKS-MANAGED-NODE"
+    }
+  }
+}
