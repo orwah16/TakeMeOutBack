3e47bf74ced824b27546403496a820710067ee67
orwah <orwah_wattad@hotmail.com>
2024-02-18 19:18:12 +0200

    merge

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    # kubernetes = {
-    #   source  = "hashicorp/kubernetes"
-    #   version = ">= 2.10"
-    # }
   }
 }
 # Configure the AWS Provider
@@ -17,3 +13,33 @@ provider "aws" {
   profile                  = "default"
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name = "argocd"
+
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "3.35.4"
+
+  values = [file("values/argocd.yaml")]
+
+}
+#upgrade for later:
+# provider "helm" {
+#   kubernetes {
+#     host                   = var.cluster_endpoint
+#     cluster_ca_certificate = base64decode(var.cluster_ca_cert)
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+#       command     = "aws"
+#     }
+#   }
+# }
