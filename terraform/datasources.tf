a25c49974ccab23354de89d381cf43551bf941bf
orwah <orwah_wattad@hotmail.com>
2024-03-14 14:27:18 +0200

    grafana chart using terraform

--- terraform/datasources.tf
+++ terraform/datasources.tf
@@ -1,29 +1,29 @@
-data "aws_ami" "server_ami" {
-  most_recent = true
-  owners      = ["137112412989"]
+# data "aws_ami" "server_ami" {
+#   most_recent = true
+#   owners      = ["137112412989"]
 
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023.3.20231218.0-kernel-6.1-arm64"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["al2023-ami-2023.3.20231218.0-kernel-6.1-arm64"]
+#   }
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
+#   filter {
+#     name   = "root-device-type"
+#     values = ["ebs"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
 data "aws_ami" "bastion_ami" {
   most_recent = true
   owners      = ["137112412989"]
 
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.3.20231218.0-kernel-6.1-x86_64"]
+    values = ["al2023-ami-2023.3.20240312.0-kernel-6.1-x86_64"]
   }
 
   filter {
